e7dbf10952fb58d6612163c019e618883766a57e
Wuda <>
2020-02-25 14:59:25 +0800

    創建套件庫

--- app/src/main/java/com/example/xbblivekotlin/MainActivity.kt
+++ app/src/main/java/com/example/xbblivekotlin/MainActivity.kt
@@ -2,11 +2,15 @@ package com.example.xbblivekotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.xbblivekotlin.MyKotlin
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var test = MyKotlin()
+        test.myTest()
     }
 }
---
+++ app/xbblivekotlin/src/main/java/com/example/xbblivekotlin/MyKotlin.kt
@@ -0,0 +1,7 @@
+package com.example.xbblivekotlin
+
+class MyKotlin {
+    public fun myTest(){
+        println("成功拉***********************")
+    }
+}
